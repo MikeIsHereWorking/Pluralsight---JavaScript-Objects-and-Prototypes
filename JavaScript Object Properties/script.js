3fd69acfc526c50782d75aeae7949d4b1f819655
Mike Bova <bova_m@subway.com>
2019-01-06 16:15:33 -0500

    Finish Lesson 2

--- JavaScript Object Properties/script.js
+++ JavaScript Object Properties/script.js
@@ -27,10 +27,56 @@
 //////////////////////////////////////////////
 // // Lesson 2- Using the Writable Attribute
 
+// var cat = {
+//     name: 'Fluffy',
+//     color: 'White'
+// }
+// Object.defineProperty(cat, 'name', {writable: false});
+// cat.name = 'Scratchy';
+// display(Object.getOwnPropertyDescriptor(cat, 'name'));
+
+//////////////////////////////////////////////
+// // Lesson 2- Using the Enumerable Attribute
+
+// var cat = {
+//     name: {first:'Fluffy', last: 'LaBeouf'},
+//     color: 'White'
+// }
+
+// Object.defineProperty(cat, 'name', {enumerable:false});
+
+// for (const propertyName in cat) {
+//     display(propertyName + ': ' + cat[propertyName])
+// }
+
+//////////////////////////////////////////////
+// // Lesson 2- Using the Configurable Attribute
+
+// Can not change the Enumerable Attribute
+// Can not change the Configurable Attribute
+// Can not delete a property
+
+//////////////////////////////////////////////
+// // Lesson 2- Getters and Setters
+
 var cat = {
-    name: 'Fluffy',
+    name: {first:'Fluffy', last: 'LaBeouf'},
     color: 'White'
 }
-Object.defineProperty(cat, 'name', {writable: false});
-cat.name = 'Scratchy';
-display(Object.getOwnPropertyDescriptor(cat, 'name'));
+
+Object.defineProperty(cat, 'fullName',
+{
+    get : function () {
+        return this.name.first + ' ' + this.name.last
+    },
+    set: function(value) {
+        var nameParts = value.split(' ');
+        this.name.first = nameParts[0];
+        this.name.last = nameParts[1]; 
+    }
+})
+
+cat.fullName = 'Muffin Top';
+display(cat.fullName);
+display(cat.name.first)
+display(cat.name.last)
